dea0c669ae86985cdf4b80281afefc455e4cd55f
LeoDuconge <leo.duconge@meteo.fr>
2022-05-19 13:52:58 +0200

    Ajout lecture TKE MNH + lecture niveaux variables avec DZ=4m

--- lecture_mesoNH.py
+++ lecture_mesoNH.py
@@ -46,8 +46,9 @@ def mesoNH(start_day, end_day, models, params):
                         '00.nc')
 
                     groupMEAN = '/LES_budgets/Mean/Cartesian/Not_time_averaged/Not_normalized/cart/'
+                    groupSBG  = '/LES_budgets/Subgrid/Cartesian/Not_time_averaged/Not_normalized/cart/'
                     groupSURF = '/LES_budgets/Surface/Cartesian/Not_time_averaged/Not_normalized/cart/'
-                    groupRAD = '/LES_budgets/Radiation/Cartesian/Not_time_averaged/Not_normalized/cart/'
+                    groupRAD  = '/LES_budgets/Radiation/Cartesian/Not_time_averaged/Not_normalized/cart/'
 
                     if 'time' not in data_mnh[today_str]:
                         data_mnh[today_str]['time'] = {}
@@ -72,35 +73,36 @@ def mesoNH(start_day, end_day, models, params):
                             if param not in data_mnh[today_str][model]:
                                 data_mnh[today_str][model][param] = {}
                                 if param == "tmp_2m":
-                                    P = f[groupMEAN].variables['MEAN_PRE'][:, 2]
+                                    P = f[groupMEAN].variables['MEAN_PRE'][:, 1]
                                     D = (100000 / P)**(2 / 7)
                                     data_mnh[today_str][model][param] = (
-                                        f[groupMEAN].variables['MEAN_TH'][:, 2] / D) - 273.15
+                                        f[groupMEAN].variables['MEAN_TH'][:, 1] / D) - 273.15
 
                                 if param == "tmp_10m":
-                                    P = f[groupMEAN].variables['MEAN_PRE'][:, 5]
+                                    P = f[groupMEAN].variables['MEAN_PRE'][:, 3]
                                     D = (100000 / P)**(2 / 7)
                                     data_mnh[today_str][model][param] = (
-                                        f[groupMEAN].variables['MEAN_TH'][:, 5] / D) - 273.15
+                                        f[groupMEAN].variables['MEAN_TH'][:, 3] / D) - 273.15
                                 if param == "hum_rel":
-                                    data_mnh[today_str][model][param] = f[groupMEAN].variables['MEAN_REHU'][:, 5]
+                                    data_mnh[today_str][model][param] = f[groupMEAN].variables['MEAN_REHU'][:, 1]
 
                                 if param == "vent_ff10m":
-                                    data_mnh[today_str][model][param] = f[groupMEAN].variables['MEANWIND'][:, 5]
+                                    data_mnh[today_str][model][param] = f[groupMEAN].variables['MEANWIND'][:, 3]
                                 if param == "flx_mvt":
                                     data_mnh[today_str][model][param] = f[groupSURF].variables['Ustar'][:]
                                 if param == "flx_chaleur_sens":
                                     data_mnh[today_str][model][param] = f[groupSURF].variables['Q0'][:] * \
-                                        f.variables['RHODREF'][2] * 1004
+                                        f.variables['RHODREF'][1] * 1004
                                 if param == "flx_chaleur_lat":
                                     data_mnh[today_str][model][param] = f[groupSURF].variables['E0'][:] * \
-                                        f.variables['RHODREF'][2] * \
+                                        f.variables['RHODREF'][1] * \
                                         2400000  # TODO facteur a corriger
                                 if param == "LWU":
-                                    data_mnh[today_str][model][param] = f[groupRAD].variables['LWU'][:, 2]
+                                    data_mnh[today_str][model][param] = (f[groupRAD].variables['LWU'][:, 1]+f[groupRAD].variables['LWU'][:, 2])/2
                                 if param == "SWD":
-                                    data_mnh[today_str][model][param] = f[groupRAD].variables['SWD'][:, 2]
-
+                                    data_mnh[today_str][model][param] = (f[groupRAD].variables['SWD'][:, 1]+f[groupRAD].variables['SWD'][:, 2])/2
+                                if param == "tke":
+                                    data_mnh[today_str][model][param] = (f[groupSBG].variables['SBG_TKE'][:, 1]+f[groupSBG].variables['SBG_TKE'][:, 2])/2
 # TODO paramètres inconnus dans les fichiers 00.000.nc
 #                                if param == "t_surface":
 #                                    f = nc.Dataset('/cnrm/ktrm/stagiaire/mosai_2021/DEV/MESONH/2021010400/MESONH_Gt_2021010400.nc')
@@ -144,6 +146,7 @@ def mesoNH_user(start_day, end_day, id_user, params):
                     '.nc')
 
                 groupMEAN = '/LES_budgets/Mean/Cartesian/Not_time_averaged/Not_normalized/cart/'
+                groupSBG  = '/LES_budgets/Subgrid/Cartesian/Not_time_averaged/Not_normalized/cart/'
                 groupSURF = '/LES_budgets/Surface/Cartesian/Not_time_averaged/Not_normalized/cart/'
                 groupRAD = '/LES_budgets/Radiation/Cartesian/Not_time_averaged/Not_normalized/cart/'
 
@@ -172,36 +175,37 @@ def mesoNH_user(start_day, end_day, id_user, params):
                         data_user[today_str][param] = {}
 
                         if param == "tmp_2m":
-                            P = f[groupMEAN].variables['MEAN_PRE'][:, 2]
+                            P = f[groupMEAN].variables['MEAN_PRE'][:, 1]
                             D = (100000 / P)**(2 / 7)
                             data_user[today_str][param] = (
-                                f[groupMEAN].variables['MEAN_TH'][:, 2] / D) - 273.15
+                                f[groupMEAN].variables['MEAN_TH'][:, 1] / D) - 273.15
 
                         if param == "tmp_10m":
-                            P = f[groupMEAN].variables['MEAN_PRE'][:, 5]
+                            P = f[groupMEAN].variables['MEAN_PRE'][:, 3]
                             D = (100000 / P)**(2 / 7)
                             data_user[today_str][param] = (
-                                f[groupMEAN].variables['MEAN_TH'][:, 5] / D) - 273.15
+                                f[groupMEAN].variables['MEAN_TH'][:, 3] / D) - 273.15
 
                         if param == "hum_rel":
-                            data_user[today_str][param] = f[groupMEAN].variables['MEAN_REHU'][:, 5]
+                            data_user[today_str][param] = f[groupMEAN].variables['MEAN_REHU'][:, 3]
 
                         if param == "vent_ff10m":
-                            data_user[today_str][param] = f[groupMEAN].variables['MEANWIND'][:, 5]
+                            data_user[today_str][param] = f[groupMEAN].variables['MEANWIND'][:, 3]
 
                         if param == "flx_mvt":
                             data_user[today_str][param] = f[groupSURF].variables['Ustar'][:]
                         if param == "flx_chaleur_sens":
                             data_user[today_str][param] = f[groupSURF].variables['Q0'][:] * \
-                                f.variables['RHODREF'][2] * 1004
+                                f.variables['RHODREF'][1] * 1004
                         if param == "flx_chaleur_lat":
                             data_user[today_str][param] = f[groupSURF].variables['E0'][:] * \
-                                f.variables['RHODREF'][2] * 2400000  # TODO facteur a corriger
+                                f.variables['RHODREF'][1] * 2400000  # TODO facteur a corriger
                         if param == "LWU":
-                            data_user[today_str][param] = f[groupRAD].variables['LWU'][:, 2]
+                            data_user[today_str][param] = (f[groupRAD].variables['LWU'][:, 1]+f[groupRAD].variables['LWU'][:, 2])/2
                         if param == "SWD":
-                            data_user[today_str][param] = f[groupRAD].variables['SWD'][:, 2]
-
+                            data_user[today_str][param] = (f[groupRAD].variables['SWD'][:, 1]+f[groupRAD].variables['SWD'][:, 2])/2
+                        if param == "tke":
+                            data_user[today_str][param] = (f[groupSBG].variables['SBG_TKE'][:, 1]+f[groupSBG].variables['SBG_TKE'][:, 2])/2
             except FileNotFoundError:
                 pass
             except TypeError:
